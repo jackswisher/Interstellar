6e6ceab1fd9922d680c304d9aa490513c5847e83
Aaron Brown <ab341@comcast.net>
2016-05-09 14:53:36 -0700

    Finished the Collision interface

--- Interstellar/src/Collision.java
+++ Interstellar/src/Collision.java
@@ -1,5 +1,20 @@
 
 public interface Collision 
 {
-
+	/**
+	 *  Returns true if the object collides with another object
+	 * @param other
+	 */
+	public boolean didCollide(Collision other);
+	
+	/**
+	 * Returns true if the object is round i.e. a Satellite
+	 * Returns false if the object is a rectangle
+	 */
+	public boolean isRound();
+	
+	/**
+	 * Returns the position of the object
+	 */
+	public Vector getPos();
 }
