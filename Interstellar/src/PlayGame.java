de341fac805342f3df8c5d022918767d85168a8a
shaylebovitz <shay.lebovitz@menloschool.org>
2016-05-11 12:56:50 -0700

    Update PlayGame

--- Interstellar/src/PlayGame.java
+++ Interstellar/src/PlayGame.java
@@ -1,11 +1,71 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class PlayGame 
+public class PlayGame implements MouseListener, KeyListener
 {
 	public static final double TIME_INTERVAL = 0.01; 
 	public static void main(String[] args) 
 	{
 		
 
+	}
+	
+	
+	public void keyPressed(KeyEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public void keyReleased(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public void keyTyped(KeyEvent e)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public void mouseClicked(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void mouseEntered(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public void mouseExited(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public void mousePressed(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void mouseReleased(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 }
